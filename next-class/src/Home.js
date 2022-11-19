d75683ec58aae53400bc7d36b5f97c0e1c691f33
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2022-11-18 21:49:52 -0500

    added home

--- next-class/src/Home.js
+++ next-class/src/Home.js
@@ -2,14 +2,8 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-    <div className="home-wrapper">  
-      <div className="home-content">
-        <div className="home-text">
-          <p>I'm currently a student in university studying to become a future software engineer. In addition, I'm passionate about learning new things and I'm always looking for new opportunities to learn and grow. Furthermore, I believe that making software more accessible is a key aspect to lead us to a better future.</p>
-        </div>
-      </div>
-    </div>
+    <div className="home-wrapper">
+        <h1>Home</h1>
     </div>
     )
   };
