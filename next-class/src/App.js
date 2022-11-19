81de7f5ac6c1e0c6b930c8409070dda0134d89ae
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2022-11-18 20:59:55 -0500

    added login screen and attempted to set up paths

--- next-class/src/App.js
+++ next-class/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginScreen from './LoginScreen';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className='Content'>
+          <Switch>
+            <Route extact path='/'>
+              <LoginScreen/>
+            </Route>
+            <Route path='/home'>
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ next-class/src/LoginScreen.js
@@ -0,0 +1,45 @@
+import './LoginScreen.css';
+
+
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const LoginScreen = () => {
+  return (
+    <div id="content">
+        <h1>Welcome!</h1>
+        <form action="" method="get">
+            <div class="input-bar">
+                <label for="name">username</label>
+                <input type="text" id="name" class="input"/>
+                <box-icon name='user'></box-icon>
+            </div>
+            <div class="input-bar">
+                <label for="password">password</label>
+                <input type="password" id="password" class="input"/>
+                <box-icon name='lock-alt' ></box-icon>
+            </div>
+        </form>
+        <button type="submit" id="btn"><Link to='/home'>Login</Link></button>
+    </div>
+  )
+}
+
+const input = document.querySelectorAll('.input');
+
+function inputFocus() {
+    this.parentNode.classList.add('focus');
+}
+
+function inputBlur() {
+    if(this.value == '' || this.value === null){
+        this.parentNode.classList.remove('focus');
+    }
+}
+
+input.forEach((e) => {
+    e.addEventListener('focus', inputFocus);
+    e.addEventListener('blur', inputBlur);
+})
+
+export default LoginScreen
