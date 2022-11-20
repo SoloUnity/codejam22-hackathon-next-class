8051074d877881ebea2bdacc85529b56fd1a738b
Gordon Ng <gordonaccount@yahoo.com>
2022-11-19 20:14:28 -0500

    Add JSON database

---
+++ next-class/src/WebService.js
@@ -0,0 +1,14 @@
+// API 
+var searchAPI = "https://api.seatalert.ca/api/mcgill/search?t=202301&q";
+var formattedAPI = "https://api.seatalert.ca/api/mcgill/formattedCourseCode?t=202301&c=MATH%20222";
+var courseAPI = "https://api.seatalert.ca/api/mcgill/course?t=202301&c=MATH-222";
+// https://api.seatalert.ca/api/mcgill/terms
+
+console.log(searchAPI);
+
+class WebService {
+
+    
+
+
+};
