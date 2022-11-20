ad1e85f89185c85327443f7d1b4914ff336919e1
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2022-11-20 10:49:40 -0500

    added logo

--- next-class/src/components/NavBar/NavBar.js
+++ next-class/src/components/NavBar/NavBar.js
@@ -2,11 +2,13 @@ import "./NavBar.css";
 import { Link } from 'react-router-dom';
 import React from 'react'
 import {auth} from '../../firebase';
+import Logo from '../../assets/Logo.png';
 
 const NavBar = () => {
   return (
     <div><div class="navbar">
     <div><h2 className="logo">Next-Class</h2></div>
+    <img src={Logo} alt='reload page' className='logo'></img>
       <div class="links">
         <button className = 'button-css' onClick={() => auth.signOut()}><Link className='link_button' to='/'>Sign Out</Link></button>
       </div>
