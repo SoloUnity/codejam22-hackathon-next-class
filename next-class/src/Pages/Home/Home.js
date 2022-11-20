9f68259185684247115ee826d617acdf75360cc5
Jione <jioneban@gmail.com>
2022-11-19 20:06:41 -0500

    Create skeleton structure

--- next-class/src/Pages/Home/Home.js
+++ next-class/src/Pages/Home/Home.js
@@ -1,14 +1,15 @@
 import './Home.css';
-import {useAuthState} from 'react-firebase-hooks/auth';
-import {auth} from '../../firebase';
 import { NavBar } from '../../components/index';
 
 const Home = () => {
-  const[user] = useAuthState(auth);
   return (
     <div className="home-wrapper">
       <NavBar />
-          <h1>Welcome {user?.email}</h1>
+
+          <div className='block-container'>
+            <div className="schedule-container">Coming Up</div>
+            <div className='friends-container'>Friends</div>
+          </div>
       </div>
     )
   };
